d5c82c2223f7a6cd0581da7ad372531642f988f4
Vivek <vivekghule777@gmail.com>
2025-10-04 21:30:54 +0530

    modify listing model

--- models/listing.models.js
+++ models/listing.models.js
@@ -1,41 +1,53 @@
-const mongoose = require('mongoose');
+const { ref } = require("joi");
+const mongoose = require("mongoose");
+const reviewModel = require("./reviews.models")
 
 let placelistingschema = new mongoose.Schema({
-    title : {
-        type: String,
-        required: true
-    },
-    description : {
-        type: String,
-        required: true
-    },
-     image: {
-    filename: {
-      type: String,
-      default: "listingimage"
-    },
-    url: {
-      type: String,
-      required: true,
-      default: "https://images.unsplash.com/photo-1755441172753-ac9b90dcd930?...",
-      set: (v) =>
-        v === ""
-          ? "https://images.unsplash.com/photo-1755441172753-ac9b90dcd930?..."
-          : v
-    }
+  title: {
+    type: String,
+    required: true,
   },
-    price : {
-        type: Number,
-        required: true
-    },
-    location : {
-        type: String,
-        required: true
-    },
-    country : {
-        type: String,
-        required: true
-    },
+  description: {
+    type: String,
+    required: true,
+  },
+
+  image: {
+    type: String,
+    required: true,
+    default:
+      "https://images.unsplash.com/photo-1470165301023-58dab8118cc9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGxvZGdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1470165301023-58dab8118cc9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGxvZGdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  reviews:[
+  
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"Review"
+  }
+  ]
+});
+
+placelistingschema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await reviewModel.deleteMany({_id: {$in: listing.reviews}})
+    
+  }
 })
 
-module.exports = mongoose.model("placelist", placelistingschema)
+module.exports = mongoose.model("placelist", placelistingschema);
