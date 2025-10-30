cb557d3bb488370e040e4ade4276a097d1be9c9d
Vivek <vivekghule777@gmail.com>
2025-10-30 19:58:33 +0530

    user

--- models/user.models.js
+++ models/user.models.js
@@ -1,20 +1,10 @@
-const mongoose = require('mongoose');
+const { required } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-let userschema = new mongoose.Schema({
-    name : {
-        type: String,
-        required: true
-    },
-    email : {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password : {
-        type: String,
-        required: true,
-    },
-   
+const userSchema = new Schema({
+    email:{
+        required:true,
+        unique
+    }
 })
-
-module.exports = mongoose.model("user", userschema)
